163dfc1b21458730a0d777c28f5b7d51144c5fe2
Vedant Ravindra Dhoke <66007382+vedant713@users.noreply.github.com>
2024-08-07 16:55:00 +0530

    Create BFS.py

---
+++ BFS-DFS/BFS.py
@@ -0,0 +1,16 @@
+import collections
+def bfs(graph,root):
+    queue=collections.deque([root])
+    visited=set()
+    while queue:
+        vertex=queue.popleft()
+        visited.add(vertex)
+        for i in graph[vertex]:
+            if i not in visited:
+            #while performing enqueue operation check if the node is not in visited 
+                queue.append(i)
+    return(visited)
+    
+
+graph={0:[1,2,3],1:[0,2],2:[0,4],3:[0],4:[2]}
+print(bfs(graph,0))
